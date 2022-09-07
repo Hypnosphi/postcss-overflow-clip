ab233492d9ac26d98162b40271fa002ab14991aa
Carlos Jeurissen <1038267+CarlosJeurissen@users.noreply.github.com>
2022-09-07 18:59:30 +0200

    Update code style

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
+'use strict';
+
 module.exports = (options = {}) => ({
   postcssPlugin: 'postcss-overflow-clip',
+
   Declaration: {
-    overflow(decl) {
-      const blockOverflowProps = decl.parent.nodes.filter(otherDecl => {
-        return otherDecl.prop === 'overflow'
+    overflow (decl) {
+      const blockOverflowProps = decl.parent.nodes.filter((otherDecl) => {
+        const isOverflowProperty = otherDecl.prop === 'overflow';
+        return isOverflowProperty;
       });
 
       // don't do anything when more than one overflow declaration is found
       if (blockOverflowProps.length > 1) return;
 
-      let propValue = decl.value;
+      const propValue = decl.value;
 
       // inject clip fallback
       if (propValue === 'clip') {
-        decl.cloneBefore({value: 'hidden'})
+        decl.cloneBefore({ value: 'hidden' });
         return;
       }
 
       // activily add clip if hidden is found
       if (propValue === 'hidden' && options.add) {
-        decl.cloneAfter({value: 'clip'})
+        decl.cloneAfter({ value: 'clip' });
       }
-    }
-  }
-})
-module.exports.postcss = true
+    },
+  },
+});
+
+module.exports.postcss = true;
